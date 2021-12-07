80198ea35a3545a6ae78c099c77fa6f91c12e0d8
Kairat <dzhumabaev.kai@gmail.com>
2021-12-07 17:03:43 +0600

    loc

--- src/views/CardLocation.vue
+++ src/views/CardLocation.vue
@@ -2,7 +2,16 @@
     <div>
         <h1>
             ID: {{this.$route.params.id}}
+
         </h1>
+        <b-list-group>
+            <b-list-group-item>Свадьба</b-list-group-item>
+            <b-list-group-item>День рождение</b-list-group-item>
+            <b-list-group-item>Корпоратив</b-list-group-item>
+            <b-list-group-item>Новый год</b-list-group-item>
+            <b-list-group-item>День зайца</b-list-group-item>
+        </b-list-group>
+
     </div>
 </template>
 
